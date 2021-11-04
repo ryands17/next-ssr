573bf3f6fe1ed26bc9d3bd0ceda34930253a8db3
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-11-05 01:16:55 +0530

    avoid updating eslint

--- pages/users.tsx
+++ pages/users.tsx
@@ -7,7 +7,11 @@ import type { Users as UsersType } from 'types'
 
 export const getServerSideProps: GetServerSideProps<{
   users: UsersType | null
-}> = async () => {
+}> = async ({ res }) => {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=600, stale-while-revalidate=1200'
+  )
   try {
     const data = await fetch('https://jsonplaceholder.typicode.com/users')
     const users = await data.json()

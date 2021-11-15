eb9f870c6856524daa645e2d9a83064a8ab7c126
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-11-15 19:30:17 +0530

    remove bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /**
  * @type {import('next').NextConfig}
  */
@@ -10,4 +6,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = nextConfig
